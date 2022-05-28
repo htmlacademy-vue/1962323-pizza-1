8c2390f3a55df4ee27c0f21a3186b3857dd66867
Дудов Павел Сергеевич <pdudov@sportmaster.ru>
2022-05-28 22:01:56 +0300

    дополнил задачу

--- src/frontend/src/common/components/__tests__/ItemCounter.spec.js
+++ src/frontend/src/common/components/__tests__/ItemCounter.spec.js
@@ -22,28 +22,9 @@ describe('ItemCounter', () => {
     wrapper.destroy();
   });
 
-  it('It sets the initial model value', () => {
-    createComponent({ propsData });
-    expect(wrapper.find('input').element.value).toBe(propsData.value);
-    expect(wrapper.find('input').element.disabled).toBe(propsData.isDisabled);
-  });
-
-  it('input type is prop type', () => {
-    createComponent({ propsData });
-    let textarea = wrapper.find('input');
-    expect(textarea.attributes('type')).toBe(propsData.type);
-  });
-
   it('input type is prop type', () => {
     createComponent({ propsData });
     let textarea = wrapper.find('input');
     expect(textarea.attributes('placeholder')).toBe(propsData.placeHolder);
   });
-
-  it('input name is prop name', () => {
-    createComponent({ propsData });
-    let textarea = wrapper.find('input');
-    expect(textarea.attributes('name')).toBe(propsData.name);
-  });
-
 });
